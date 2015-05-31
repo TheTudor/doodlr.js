572bdbd615c57f8557c327da0f79634f096aaae7
JCGrant <jamescolin.grant@gmail.com>
2015-05-31 20:35:01 +0100

    Can login and register with parse in meteor!

--- scribblr/scribblr.js
+++ scribblr/scribblr.js
@@ -1,4 +1,8 @@
+
+Parse.initialize("9QOijSH3c8VZ4OMuXSNtcyZ9DOlNCttX9iMsv1GL", "mbIy8g11RvZG6c2hoZ9IHumiEGszjWyACcaOcsHg");
+
 if (Meteor.isClient) {
+  
   Template.body.helpers({
     rows: function() {
       var rows = [];
@@ -13,4 +17,49 @@ if (Meteor.isClient) {
     }
   });
 
+  Template.registerForm.events({  
+    'submit form': function(event) {
+
+      event.preventDefault();
+
+      var username = event.target.username.value;
+      var email = event.target.email.value;
+      var password = event.target.password.value;
+    
+      user = new Parse.User()
+
+      user.set("username", username);
+      user.set("password", password);
+      user.set("email", email);
+
+      user.signUp(null, {
+        success: function(user) {
+          console.log("User registered.");
+        },
+        error: function(user, error) {
+          console.log("Error: " + error.code + " " + error.message);
+        }
+
+      });
+    }
+  });
+
+  Template.logInForm.events({  
+    'submit form': function(event) {
+
+      event.preventDefault();
+
+      var username = event.target.username.value;
+      var password = event.target.password.value;
+      
+      Parse.User.logIn(username, password, {
+        success: function(user) {
+          console.log("User logged in.");
+        },
+        error: function(user, error) {
+          console.log("Error: " + error.code + " " + error.message);
+        }
+      });
+    }
+  });
 }
