db240db8a5f2f72de0defe8579e79a3d7e3593be
mearlboro <ms6413@ic.ac.uk>
2015-06-01 14:01:23 +0100

    Attempt to integrate canvas with meteor in meteor_canvas. Removed color picker library (to be replaced with meteor package). Mouse events not working

---
+++ meteor_canvas/meteor_canvas.js
@@ -0,0 +1,111 @@
+if (!Meteor.isClient) return;
+
+this.Canvas = function Canvas(id) {
+  // -- Canvas ------------------------------------------------------- //
+  var canvas = document.getElementById('canvas');
+  ctx = canvas.getContext("2d");
+  w = canvas.width;
+  h = canvas.height;
+  draw_flag = false,
+    previousX = 0,
+    currentX = 0,
+    previousY = 0,
+    currentY = 0;
+
+  // Drawing parameters
+  var color = "#000",
+    size = 2;
+
+  // canvas.addEventListener("mousemove", function (e) { get_coordinates('move', e) }, false);
+  // canvas.addEventListener("mousedown", function (e) { get_coordinates('down', e) }, false);
+  // canvas.addEventListener("mouseup",   function (e) { get_coordinates('up'  , e) }, false);
+  // canvas.addEventListener("mouseout",  function (e) { get_coordinates('out' , e) }, false);
+
+  Template.Canvas.events({
+    'mousedown': function(e) {
+      get_coordinates('down', e);
+    },
+    'mouseup': function(e) {
+      get_coordinates('up', e);
+    },
+    'mousemove': function(e) {
+      get_coordinates('move', e);
+    },
+    'mouseout': function(e) {
+      get_coordinates('out', e);
+    }
+  });
+
+
+  // -- Draw --------------------------------------------------------- //
+  // Get color from jscolor color picker
+  function pick_color(obj) {
+    color = '#' + obj.color;
+  }
+ 
+  // Draws a dot at coordinates (currentX, currentY) 
+  // with lineWidth of size @size and strokeStyle of color @color
+  function draw_dot() {
+    ctx.beginPath();
+
+    ctx.fillStyle = color;
+    ctx.fillRect(currentX, currentY, size, size);
+
+    ctx.closePath();
+  }
+
+  // Draws a line between coordinates (previousX, previousY), (currentX, currentY) 
+  // with lineWidth of size @size and strokeStyle of color @color
+  function draw_line() {
+    ctx.beginPath();
+
+    ctx.moveTo(previousX, previousY);
+    ctx.lineTo(currentX, currentY);
+    ctx.lineWidth   = size;
+    ctx.strokeStyle = color;
+
+    ctx.stroke();
+    ctx.closePath();
+  }
+
+  // Gets coordinates of mouse click and performs corresponding action
+  // Actual drawing happens here 
+  function get_coordinates(action, e) {
+    // On mousedown, draw first dot on location
+    if (action == 'down') {
+      previousX = currentX;
+      previousY = currentY;
+      currentX = e.clientX - canvas.offsetLeft;
+      currentY = e.clientY - canvas.offsetTop;
+
+      draw_flag = true;
+      if (draw_flag) {
+        draw_dot();
+      }
+    }
+    // On mouse up or mouse out, no action (can't draw outside canvas)
+    if (action == 'up' || action == "out") {
+      draw_flag = false;
+    }
+    // On mousemove, draw line between move coordinates
+    if (action == 'move') {
+      if (draw_flag) {
+          previousX = currentX;
+          previousY = currentY;
+          currentX = e.clientX - canvas.offsetLeft;
+          currentY = e.clientY - canvas.offsetTop;
+
+          draw_line();
+      }
+    }
+  }
+
+  ctx.fillRect(0, 0, canvas.width(), canvas.height());
+
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
