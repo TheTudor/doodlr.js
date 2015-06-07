4ade55ff0012aee55ae991682fe69b0bef532632
mearlboro <ms6413@ic.ac.uk>
2015-06-07 20:35:42 +0100

    Cleanup in all the files

---
+++ canvas/.meteor/local/build/programs/web.browser/app/client/routes.js
@@ -0,0 +1,7 @@
+(function(){// Documentation - https://github.com/iron-meteor/iron-router/blob/devel/Guide.md
+// the page in home.html will run at /canvas
+Router.route('/canvas', function () {
+  this.render('Canvas');
+});
+
+})();
