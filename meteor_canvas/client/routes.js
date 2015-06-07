e434e20dcf32aafe9360cfaef69590e1c73a6ab9
mearlboro <ms6413@ic.ac.uk>
2015-06-07 21:23:56 +0100

    Cleanup 2

--- meteor_canvas/client/routes.js
+++
@@ -1,5 +0,0 @@
-// Documentation - https://github.com/iron-meteor/iron-router/blob/devel/Guide.md
-// the page in home.html will run at /canvas
-Router.route('/canvas', function () {
-  this.render('Canvas');
-});
