8ee93f8ebde4ec04c906aa904ddf1ef40349e2da
tnc13 <tnc13@imperial.ac.uk>
2015-06-18 04:09:43 +0100

    Collaborately drawing with pencil and brush work!

--- client/lib/receiver.js
+++ client/lib/receiver.js
@@ -1,15 +1,35 @@
 this.Receiver = function Receiver(id){
-  console.log("receiver created");
-  
-  drawStream.on(id + ":down-dot", function(at) {
+  console.log("receiver created"); // creates receiver
+
+  // stream listeners furr each event emitter.
+  drawStream.on(id + ":down-dot", function(at, curr, pencilSize, color) {
     console.log("should be drawing dot now");
-    editor.drawDot(at.x, at.y);
+    editor.drawDefaultPencilDot(at, curr, pencilSize, color);
+  });
+  
+  drawStream.on(id + ":down-texture", function(curr, pencilSize, pencilTexture, opac, color) {
+    editor.drawTextureDot(curr, pencilSize, pencilTexture, opac, color); 
   });
 
-  drawStream.on(id + ":move-line", function(from, to) {
+
+  drawStream.on(id + ":move-line", function(prev, curr, pencilSize, color) {
     console.log("drawing line"  + id);
-    editor.drawLine(from, to);
+    editor.drawDefaultPencilLine(prev, curr, pencilSize, color)
   });
+
+  drawStream.on(id + ":move-texture", function(prev, curr, pencilSize, pencilTexture, opac, color) {
+    console.log("receiver" + pencilTexture);
+    editor.drawTextureLine(prev, curr, pencilSize, pencilTexture, opac, color); 
+  });
+
+  // ADD HERE every @id + 'eventname' being listened to
+  this.close = function() {
+    drawStream.removeAllListeners(id + ':down-dot');
+    drawStream.removeAllListeners(id + ':down-texture');
+    drawStream.removeAllListeners(id + ':move-line');
+    drawStream.removeAllListeners(id + ':move-texture');
+  }
+
 
 
 }
--- client/lib/views.js
+++ client/lib/views.js
@@ -4,6 +4,9 @@ receiver = null;
 
 Meteor.startup(function() {
   Deps.autorun(function() {
+    if(editor){
+      receiver.close();
+    }
     var currentId = Session.get('currentId');
     console.log(currentCanvas);
     editor = new Editor(currentId);
